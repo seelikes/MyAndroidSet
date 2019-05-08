0fcbcd2d82d7845f02534b70ff27f8d00b792f7a
liutiantian <Snape_Stack@Hotmail.com>
2019-05-08 15:37:41 +0800

    solve can not build success within ide or gradlew command

--- MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/business/main/MainItemBean.kt
+++ MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/business/main/MainItemBean.kt
@@ -3,8 +3,4 @@ package com.example.myjetpackapplication.business.main
 /**
  * Created by liutiantian on 2019-05-02 12:14 星期四
  */
-@Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@Repeatable
-@MustBeDocumented
-annotation class MainItemBean(val title: Int, val path: String, val enable: Boolean, val children: Array<MainItemBean> = [])
+class MainItemBean(val title: Int, val path: String, val enable: Boolean = true, val children: Array<MainItemBean> = arrayOf())
--- MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/business/main/MainViewModel.kt
+++ MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/business/main/MainViewModel.kt
@@ -8,30 +8,27 @@ import com.example.myjetpackapplication.basic.BasicHostViewModel
 import com.example.myjetpackapplication.databinding.ActivityMainBinding
 import com.example.myjetpackapplication.utils.mergeArray
 
-@MainItemBean(
+private val rootItem = MainItemBean(
     title = R.string.app_name,
     path = "/business/main",
-    enable = true,
-    children = [
+    children = arrayOf(
         MainItemBean(
             title = R.string.app_name,
             path = "/business/main",
-            enable = true,
-            children = [
+            children = arrayOf(
                 MainItemBean(
                     title = R.string.app_name,
-                    path = "/business/main",
-                    enable = true
+                    path = "/business/main"
                 )
-            ]
+            )
         ),
         MainItemBean(
             title = R.string.app_name,
-            path = "/business/main",
-            enable = true
+            path = "/business/main"
         )
-    ]
+    )
 )
+
 class MainViewModel(host: MainActivity, binding: ActivityMainBinding) : BasicHostViewModel<MainViewModel, MainActivity, ActivityMainBinding>(host, binding) {
     val items = ObservableArrayList<MainItemBean>()
 
@@ -43,6 +40,6 @@ class MainViewModel(host: MainActivity, binding: ActivityMainBinding) : BasicHos
 
     override fun afterSetToBinding() {
         super.afterSetToBinding()
-        ViewModelProviders.of(host).get(MainDataModel::class.java).items.value = javaClass.getAnnotation(MainItemBean::class.java)?.children
+        ViewModelProviders.of(host).get(MainDataModel::class.java).items.value = rootItem.children
     }
 }
