85c10f28093b848c00463e4e88834e96fa8596e9
liutiantian <Snape_Stack@Hotmail.com>
2019-05-08 17:57:00 +0800

    add immersionBar support

--- MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/basic/BasicActivity.kt
+++ MyJetPackApplication/app/src/main/java/com/example/myjetpackapplication/basic/BasicActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myjetpackapplication.basic
 
 import android.os.Bundle
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.ViewDataBinding
+import com.gyf.immersionbar.ktx.immersionBar
 
 abstract class BasicActivity<C : BasicActivity<C, M, B>, M : BasicHostViewModel<M, C, B>, B : ViewDataBinding> : AppCompatActivity(), BasicInitView<B> {
     @Suppress("MemberVisibilityCanBePrivate")
@@ -11,6 +13,12 @@ abstract class BasicActivity<C : BasicActivity<C, M, B>, M : BasicHostViewModel<
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         model = initModel(savedInstanceState)
+        if (useImmersionBar()) {
+            immersionBar {
+                statusBarColor(provideStatusBarColor())
+                navigationBarColor(provideStatusBarColor())
+            }
+        }
         initView(model.binding)
     }
 
@@ -23,4 +31,14 @@ abstract class BasicActivity<C : BasicActivity<C, M, B>, M : BasicHostViewModel<
      * 初始化model
      */
     abstract fun initModel(savedInstanceState: Bundle?): M
+
+    /**
+     * 是否采用沉浸式状态栏
+     * true 采用沉浸式状态栏
+     * false 不采用或自行实现沉浸式状态栏
+     */
+    open fun useImmersionBar(): Boolean = true
+
+    @ColorRes
+    open fun provideStatusBarColor(): Int = android.R.color.white
 }
