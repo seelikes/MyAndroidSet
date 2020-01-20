a604eaf49223323fd4bb05808551cdab919adc27
刘甜甜 <seelikes@163.com>
2020-01-20 15:10:03 +0800

    solve anr not shown in final app while compiling it's resources

--- MyJetPackApplication/businesses/business-anr/src/main/java/com/example/myjetpackapplication/business/anr/AnrActivity.kt
+++ MyJetPackApplication/businesses/business-anr/src/main/java/com/example/myjetpackapplication/business/anr/AnrActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.example.myjetpackapplication.business.anr.R
+import com.example.myjetpackapplication.annotationprocessor.business.annotation.Business
 import com.example.myjetpackapplication.business.anr.databinding.ActivityAnrBinding
 import com.github.seelikes.android.mvvm.basic.BasicActivity
 
+@Business(title = "anr_title")
 @Route(path = "/business13/anr")
 class AnrActivity : BasicActivity<AnrActivity, AnrViewModel, ActivityAnrBinding>() {
     override fun initModel(savedInstanceState: Bundle?): AnrViewModel = AnrViewModel(this, DataBindingUtil.setContentView(this, R.layout.activity_anr))
