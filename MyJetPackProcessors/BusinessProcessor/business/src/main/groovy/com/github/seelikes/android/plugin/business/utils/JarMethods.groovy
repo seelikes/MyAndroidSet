329126c6ab6ee2922483b9badf6b3a321eb0d7aa
刘甜甜 <seelikes@163.com>
2021-03-16 09:01:56 +0800

    classes.jar maybe the same across modules, should have a unique identifier

--- MyJetPackProcessors/BusinessProcessor/business/src/main/groovy/com/github/seelikes/android/plugin/business/utils/JarMethods.groovy
+++ MyJetPackProcessors/BusinessProcessor/business/src/main/groovy/com/github/seelikes/android/plugin/business/utils/JarMethods.groovy
@@ -11,7 +11,7 @@ import org.gradle.api.Project
  */
 trait JarMethods {
     void copyJar(Project project, TransformInvocation transformInvocation, JarInput jarInput, File jarFile) {
-        def jarName = jarFile.name
+        def jarName = jarFile.absolutePath.startsWith(project.rootDir.absolutePath) ? jarFile.absolutePath.substring(project.rootDir.absolutePath.length() + 1) : jarFile.absolutePath
         def dest = transformInvocation.outputProvider.getContentLocation(jarName, jarInput.contentTypes, jarInput.scopes, Format.JAR)
         FileUtils.copyFile(jarFile, dest)
     }
